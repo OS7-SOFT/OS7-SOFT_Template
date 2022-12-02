8161bb9a08021728ae12e8f9d3c86686ca59bcf2
Osama Yeslam <os7softgithub@gmail.com>
2022-12-02 19:46:14 +0300

    Update main.css and main.js file

--- Js/main.js
+++ Js/main.js
@@ -3,9 +3,11 @@ let ourSkill_section = document.getElementById("our-skill");
 let spanProgress = document.querySelectorAll(".prog span");
 let spanCount = document.querySelectorAll("h4 span");
 
+let scrollUp = document.querySelector(".scroll-up");
+
 function ShowSpanCount() {
   spanCount.forEach((span) => {
-    span.style.display = "block";
+    span.style.opacity = 1;
   });
 }
 window.onscroll = function () {
@@ -15,4 +17,17 @@ window.onscroll = function () {
     });
     setTimeout(ShowSpanCount, 1000);
   }
+
+  if (window.scrollY >= 1000) {
+    scrollUp.style.display = "block";
+  } else {
+    scrollUp.style.display = "none";
+  }
 };
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
