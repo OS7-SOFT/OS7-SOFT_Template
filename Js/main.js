be26f7494f5e423f40b5319e701d3c27213d64ee
Osama Yeslam <os7softgithub@gmail.com>
2022-12-02 17:07:03 +0300

    Update main.css and main.js file

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,18 @@
 let ourSkill_section = document.getElementById("our-skill");
 
 let spanProgress = document.querySelectorAll(".prog span");
+let spanCount = document.querySelectorAll("h4 span");
 
+function ShowSpanCount() {
+  spanCount.forEach((span) => {
+    span.style.display = "block";
+  });
+}
 window.onscroll = function () {
   if (window.scrollY >= ourSkill_section.offsetTop - 300) {
     spanProgress.forEach((span) => {
       span.style.width = span.dataset.width;
     });
+    setTimeout(ShowSpanCount, 1000);
   }
 };
